17498c3c1ed467259f25f736804cd214917f5870
Amira el Manaa <amiraelmanaa13@gmail.com>
2025-04-03 19:48:11 +0200

    login and register

--- ex4/loginpage.php
+++ ex4/loginpage.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require "user.php";
 
 $host = "sql7.freesqldatabase.com";
 $dbname = "sql7771121";
@@ -70,6 +71,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
          <?php endif; ?>
             <button type="submit" class="btn btn-primary">Se connecter</button>
         </form>
+
+        <div class="mt-3 text-center">
+            <a href="register.php">Créer un compte</a>
+            
+
+
+
+
     </div>
 </body>
 </html>
